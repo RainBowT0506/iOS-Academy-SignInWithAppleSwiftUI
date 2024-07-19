b8c70d4c4a582e008e0e0df57aadff01544b62b9
RainBowT0506 <rainbowt0506@gmail.com>
2024-07-20 04:52:40 +0800

    Initial Commit

---
+++ iOS Academy-SignInWithAppleSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS Academy-SignInWithAppleSwiftUI
+//
+//  Created by RainBowT on 2024/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS Academy-SignInWithAppleSwiftUI/iOS_Academy_SignInWithAppleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Academy_SignInWithAppleSwiftUIApp.swift
+//  iOS Academy-SignInWithAppleSwiftUI
+//
+//  Created by RainBowT on 2024/7/20.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Academy_SignInWithAppleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
